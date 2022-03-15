015dcc0648109d5b2051b17292f4fed1d142c1b4
makarasty <71918286+makarasty@users.noreply.github.com>
2022-03-15 23:31:16 +0300

    updata

--- trash/js/script.js
+++ trash/js/script.js
@@ -1,12 +1,12 @@
  //антикопирка
 window.onkeydown = function(evt) {
-    //if (evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false
+    if (evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false
 };
 window.onkeypress = function(evt) {
-    //if (evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false;
+    if (evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false;
 };
 function r(e) {
-    //if ((e.which || e.keyCode) == 116) e.preventDefault();
+    if ((e.which || e.keyCode) == 116) e.preventDefault();
 };
 //$(document).on("keydown", r);
 document.ondragstart = noselect;
