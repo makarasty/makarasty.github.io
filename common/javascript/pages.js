238108be1858935020c59673a8c060b4d3bbe041
MaKarastY <71918286+makarasty@users.noreply.github.com>
2024-03-27 19:10:22 +0200

    Update

---
+++ common/javascript/pages.js
@@ -0,0 +1,25 @@
+const blockedKeysForPages = new Set(['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F12', 'Alt']);
+
+/**
+ * @param {Event} event 
+ */
+function breakEvent(event) {
+	event.preventDefault()
+	return false
+}
+
+document.addEventListener('dragstart', breakEvent);
+document.addEventListener('selectstart', breakEvent);
+document.addEventListener('contextmenu', breakEvent);
+
+/**
+ * @param {KeyboardEvent} event 
+ * @returns 
+*/
+function onKeyDownEvent(event) {
+	if (blockedKeysForPages.has(event.key.toUpperCase())) {
+		return breakEvent(event)
+	}
+}
+
+document.addEventListener('keydown', onKeyDownEvent);
